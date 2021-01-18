453322b13651d8fc056db5c341f07d83bbc6d0af
root <root@backup.paedml-linux.lokal>
2021-01-18 16:22:47 +0100

    Updated Readme, fixed bugs...

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 This scripts from opsi hardware database the networkcontrollers description. If it's finding 'wireless' in this field it will add the mac and dns entry on udm server.
 
 ## HowTo run
-run script with "python wlan-mac-adding.py"
+run script with "python wlan-mac-adding.py" on opsi server(backup) as root
--- wlan-mac-adding.py
+++ wlan-mac-adding.py
@@ -1,13 +1,13 @@
 #!/bin/python
-#from __future__ import print_function
+from __future__ import print_function
 import os
 import sys
 import pprint
 import time
-#from OPSI.Backend.BackendManager import BackendManager
+from OPSI.Backend.BackendManager import BackendManager
 
-#backend = BackendManager()
-#clients = backend.host_getObjects(type="OpsiClient")
+backend = BackendManager()
+clients = backend.host_getObjects(type="OpsiClient")
 # future plan maybe executing on server and access jsonrpcbackend remotely
 #from OPSI.Backend.JSONRPC import JSONRPCBackend
 #b = JSONRPCBackend(address="https://backup.paedml-linux.lokal:4447/rpc", username="administrator", password="xxxx")
@@ -15,15 +15,11 @@ import time
 
 print('You need to have the opsi product hwaudit installed on the client to make this work!')
 print('Please check if \'ssh root@server.paedml-linux.lokal\' is working from backup without errors!')
-#raw_input('Press enter if you read info above and start the script.....')
+raw_input('Press enter if you read info above and start the script.....')
 
 # get all hosts and macs(if already added)
-#allHosts = os.popen('ssh root@server.paedml-linux.lokal \'udm computers/windows list\'').read()
-with open('testi.txt', "r") as file_object:
-    data = file_object.read()
-    file_object.close()
-
-allHosts = data.split('DN:')
+allHosts = os.popen('ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=quiet root@server.paedml-linux.lokal \'udm computers/windows list\'').read()
+allHosts = allHosts.split('DN:')
 dictHosts = {}
 
 for host in allHosts:
@@ -33,8 +29,7 @@ for host in allHosts:
   hosti = host[host.find(start)+len(start):host.find(end)]
   # check if 2 dhcpentryzones, if so, 2 macs....if len = 3, 2 dpchpentries...
   calcMacs = host.split('dhcpEntryZone:')
-  dictHosts[hosti] = calcMacs
-sys.exit()
+  dictHosts[hosti] = str(len(calcMacs))
 
 for client in clients:
   # checking 1
@@ -45,13 +40,13 @@ for client in clients:
   ak =  backend.getHardwareInformation_hash(client.id)
   for networkcontroller in ak[u'NETWORK_CONTROLLER']:
       if 'wireless' in networkcontroller[u'description'].lower():
-          print("Checking MAC and DNS of  " + client.id + ": " + networkcontroller[u'macAddress'] + "...")#,end="")
           # check if second mac is already in allHosts
           clientName = client.id.split('.')
-          if not dictHosts[clientName[0] = str('3'): 
+          if not dictHosts[clientName[0]] == '3': 
             hashClient = backend.host_getHashes(id=client.id)
-            sshCommandFirst = 'ssh root@server.paedml-linux.lokal \'udm computers/windows modify --dn=cn=' + clientName[0] + ',cn=computers,ou=schule,dc=paedml-linux,dc=lokal --append mac='+  networkcontroller[u'macAddress'] + ' --append "dhcpEntryZone=cn=schule,cn=dhcp,ou=schule,dc=paedml-linux,dc=lokal '+ hashClient[0]['ipAddress'] +' ' + networkcontroller[u'macAddress'] +'"\''
-            os.command(sshCommandFirst)
+            print("Checking MAC and DNS of " + client.id + ": " + networkcontroller[u'macAddress'] + "...")
+            sshCommandFirst = 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=quiet root@server.paedml-linux.lokal \'udm computers/windows modify --dn=cn=' + clientName[0] + ',cn=computers,ou=schule,dc=paedml-linux,dc=lokal --append mac='+  networkcontroller[u'macAddress'] + ' --append "dhcpEntryZone=cn=schule,cn=dhcp,ou=schule,dc=paedml-linux,dc=lokal '+ hashClient[0]['ipAddress'] +' ' + networkcontroller[u'macAddress'] +'"\''
+            os.system(sshCommandFirst)
             time.sleep(2)
 
 print('End of filling wlan macs on server...')
